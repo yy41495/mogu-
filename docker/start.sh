c4b228f0d6d162b33bad144eda7692e75ddff55e
yy414 <itkc41495@gmail.com>
2026-02-28 16:06:33 +0900

    500エラー修正

--- docker/start.sh
+++ docker/start.sh
@@ -2,8 +2,5 @@
 php artisan config:cache
 php artisan route:cache
 php artisan migrate --force
-php artisan storage:link
-chmod -R 775 /var/www/html/storage
-chmod -R 775 /var/www/html/bootstrap/cache
 php-fpm -D
 nginx -g "daemon off;"
