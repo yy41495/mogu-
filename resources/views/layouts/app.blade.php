f3f0972e8d79e1c1b6753398b97ade96802a0508
yy414 <itkc41495@gmail.com>
2026-04-11 13:56:31 +0900

    デザイン微修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <link rel="icon" href="/favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'mogu+')</title>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;600;700&display=swap" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://unpkg.com/lucide@latest"></script>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @vite(['resources/css/base.css'])
     @yield('css')
     @vite(['resources/css/responsive.css'])
 </head>
+
 <body>
     <!-- ヘッダー -->
     <div class="header">
@@ -32,4 +36,5 @@
         lucide.createIcons();
     </script>
 </body>
+
 </html>
